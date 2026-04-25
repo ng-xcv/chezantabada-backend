f54ee0c382f4775f83d7bf8cf6ab7c53c96e2ac3
ngxcv <ngaryfaye95@gmail.com>
2026-04-25 00:53:51 +0000

    fix(cors): allow antabada.shop origins

--- src/server.js
+++ src/server.js
@@ -22,6 +22,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://chezantabada-frontend.vercel.app',
+  'https://antabada.shop',
+  'https://www.antabada.shop',
   process.env.FRONTEND_URL,
 ].filter(Boolean)
 
